62530e1b48d8a1a757d6f1f7757cde2bd79ff28d
Michael Satterfield <michael.f.satterfield@gmail.com>
2022-01-23 17:01:22 -0600

    friends object

--- server.js
+++ server.js
@@ -19,9 +19,20 @@ const friends = [
 
 //route handlers
 app.get("/friends", (req, res) => {
-  res.send(friends);
+  res.json(friends);
 });
 
+app.get("/friends/:id", (req,res) => {
+const id = Number(req.params.id);
+const friend = friends[id];
+//if friend is found return the friend json object
+if(friend) {
+    res.json(friend);
+} else {
+    res.status(404).json({
+        error: "This friend does not exist"
+    })
+}
 app.get("/2", (req, res) => {
   res.send("<h1>hello tester person!</h1>");
 });
