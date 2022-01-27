a43a77cba6e22d5065649c82f949ee3505e4922d
Michael Satterfield <michael.f.satterfield@gmail.com>
2022-01-27 12:42:46 -0600

    adding basic front end to be served by express

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -2,7 +2,9 @@ const path = require("path");
 
 //use named function at top level for exporting ect for debugging purposes!
 function getMessage(req, res) {
-  res.sendFile(path.join(__dirname, "..", "public", "skimountain.jpg"));
+  res.sendFile(
+    path.join(__dirname, "..", "public", "images", "skimountain.jpg")
+  );
   //   res.send("<h1>hello tester person!</h1>");
 }
 
